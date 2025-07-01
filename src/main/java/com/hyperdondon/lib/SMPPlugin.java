0906b093e669789fa10ad11f7f493b4c0700a57d
Daniel Abu Dawoud <107346732+HyperDondon@users.noreply.github.com>
2025-07-01 15:59:41 -0400

    check desc
    remove the stupid sphericalvector class and replace it with a superior utility.
    use my own ComponentWrapper class that supports legacy (ampersand and section sign) and mini message formats.
    remove the special instance stuff in the main class.

--- src/main/java/com/hyperdondon/lib/SMPPlugin.java
+++ src/main/java/com/hyperdondon/lib/SMPPlugin.java
@@ -1,53 +1,9 @@
 package com.hyperdondon.lib;
 
-import com.hyperdondon.lib.powers.cooldown.system.PowerSelectionSystem;
-import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.mineacademy.fo.plugin.SimplePlugin;
 
-import java.util.Objects;
-
 public abstract class SMPPlugin extends SimplePlugin {
-
-    private static SMPPlugin instance;
-
-    public static SMPPlugin getInstance() {
-        if (instance == null) {
-            try {
-                instance = (SMPPlugin) JavaPlugin.getPlugin(SMPPlugin.class);
-            } catch (IllegalStateException ex) {
-                if (Bukkit.getPluginManager().getPlugin("PlugMan") != null) {
-                    Bukkit.getLogger().severe("Failed to get instance of the plugin, if you reloaded using PlugMan, you need to do a clean restart instead.");
-                }
-
-                throw ex;
-            }
-
-            Objects.requireNonNull(instance, "Cannot get a new instance! Have you reloaded?");
-        }
-
-        return instance;
-    }
-
-    public Component MiniMessage(String text) {return MiniMessage.miniMessage().deserialize(text);}
-
-    public Component MiniMessage(String... texts) {return MiniMessage(String.join("\n", texts));}
-
-    public String Serialize(Component component) {return LegacyComponentSerializer.legacySection().serialize(component);}
-
-    public String Serialize(Component... components) {
-        String[] texts = new String[components.length];
-        for (int i = 0; i < components.length; i++) texts[i] = Serialize(components[i]);
-        return String.join("\n", texts);
-    }
-
-    public final void onReloadablesStart() {
-        this.onPluginEnable();
-    }
-
-    protected abstract void onPluginEnable();
 }
--- src/main/java/com/hyperdondon/lib/item/ItemConstructor.java
+++ src/main/java/com/hyperdondon/lib/item/ItemConstructor.java
@@ -2,6 +2,7 @@ package com.hyperdondon.lib.item;
 
 
 import com.hyperdondon.lib.SMPPlugin;
+import com.hyperdondon.lib.util.format.ComponentWrapper;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -10,15 +11,18 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.UUID;
 
 public class ItemConstructor {
-    public static ItemStack construct(Component display, int cmd, Material material, String type, UUID id, Component... lore) {
+    public static ItemStack construct(ComponentWrapper display, int cmd, Material material, String type, UUID id, ComponentWrapper... lore) {
         ItemStack itemStack = new ItemStack(material);
         ItemMeta itemMeta = itemStack.getItemMeta();
-        itemMeta.displayName(display);
-        itemMeta.lore(Arrays.stream(lore).toList());
+        itemMeta.displayName(display.getComponent());
+        ArrayList<Component> componentListLore = new ArrayList<>();
+        for (ComponentWrapper line : lore) componentListLore.add(line.getComponent());
+        itemMeta.lore(componentListLore.stream().toList());
         itemMeta.setCustomModelData(cmd);
         itemMeta.getPersistentDataContainer().set(key("type"), PersistentDataType.STRING, type);
         itemMeta.getPersistentDataContainer().set(key("id"), PersistentDataType.STRING, id.toString());
